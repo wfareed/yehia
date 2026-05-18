b56484fa05544c14091cd0d181fabf775c32676b
wfareed <wfareed.admin@gmail.com>
2026-05-18 18:39:45 +0300

    Next upgrage
    upgraded Netx.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com', 'flagcdn.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'via.placeholder.com' },
+      { protocol: 'https', hostname: 'flagcdn.com' },
+    ],
   },
 }
 
